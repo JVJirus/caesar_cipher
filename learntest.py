b91538e6b87a0df4c3a5446febad1bb003984a1c
JVJirus <valentina.mazzara.1301@gmail.com>
2023-11-24 21:59:50 +0100

    finished the encrypt function with ascii table

--- learntest.py
+++ learntest.py
@@ -1,16 +1,18 @@
 def encrypt(sentence,key):
-    #alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"] old
-    alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
-    #alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z","1","2","3","4","5","6","7","8","9","0"] Test to see if it sees numbers now, ok no like this it also applies the key to numbers
     result = f""
-    for element in sentence :
-        if element.isalpha() and element in alphabet :
-           elem_index = alphabet.index(element)
-           new_index = (elem_index + key) % len(alphabet)
-           new_letter = alphabet[new_index]
-           result = f"{result}{new_letter}"
+    for element in sentence:
+        conversion = ord(element)
+        
+        if conversion >= 65 and conversion <= 90:
+           new_index = (conversion - 65 + key) % 26 + 65
+        elif conversion >= 97 and conversion <= 122:
+           new_index = (conversion - 97 + key) % 26 + 97
         else:
-            result = f"{result}{element}"    
+            new_index = conversion
+            
+            
+        new_letter = chr(new_index)
+        result = f"{result}{new_letter}"    
     
     return result    
 
@@ -26,3 +28,4 @@ Text = input("Your string here ") #We want to read the input
 #test = test.upper() #Just to guarantee the function finds the correct letter, downside is all results will all be uppercase / obsolete cuz we fixed it by adding lower uppcase letter in list
 result = encrypt(Text,1)
 print(result)
+
